75016030d68b73af60282ea77c80f1806cec5f96
Brad Chapman <chapmanb@50mail.com>
2014-11-26 02:12:38 +0000

    Provide full breakdowns of replicates with copy details and basic filters

--- scripts/evaluate_cnvs.py
+++ scripts/evaluate_cnvs.py
@@ -14,18 +14,40 @@ def main(config_file):
         config = yaml.load(in_handle)
     sample_cnvs = read_cnvs(tz.get_in(["inputs", "cnvs"], config),
                             tz.get_in(["inputs", "namere"], config), config["remap"])
-    for sample, cnvs in sample_cnvs.iteritems():
+    for sample, cnvs in sorted(sample_cnvs.iteritems()):
         summarize_cnvs(sample, cnvs)
 
 def summarize_cnvs(sample, cnvs):
     by_replicate = collections.defaultdict(list)
     for cnv in cnvs:
         by_replicate[cnv["SAMPLE_ID"]].append(cnv)
-    print "---", sample
     rep_names = sorted(by_replicate.keys())
-    for rep_name in rep_names:
-        items = by_replicate[rep_name]
-        print " ", rep_name, len(items)
+    if len(rep_names) > 1:
+        print "---", sample
+        total_count = sum([len(xs) for xs in by_replicate.itervalues()])
+        if total_count < 100:
+            _examine_replicates(by_replicate, rep_names)
+        else:
+            for rep_name in rep_names:
+                items = by_replicate[rep_name]
+                print " ", rep_name, len(items)
+
+def _examine_replicates(cnvs_by_rep, rep_names):
+    """Examine CNV calls organized by replicates.
+    """
+    for rep in rep_names:
+        print " ", rep
+        for cnv in cnvs_by_rep[rep]:
+            if ((cnv["status"] == "filtered" and int(cnv["CopyNumber"]) > 2)
+                  or float(cnv["mean.base.spacing"]) > 2.8):
+                flag = " ColorFilter"
+            else:
+                flag = ""
+            print "  %s %s %s %s %s%s" % (cnv["chr.id"], cnv["start"], cnv["end"], cnv["CopyNumber"],
+                                          cnv["size"], flag)
+            if False:
+                import pprint
+                pprint.pprint(cnv)
 
 def read_cnvs(in_file, name_re, remap):
     """Read CNV information from input file and group by sample.
